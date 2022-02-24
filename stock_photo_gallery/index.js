968b57c043bd8cc8c79f763dd184b41945d33fac
Jonathan Sexton <jonathan.sexton@cloudinary.com>
2022-02-24 16:41:08 -0600

    updated

--- stock_photo_gallery/index.js
+++ stock_photo_gallery/index.js
@@ -1,33 +1,31 @@
-const fs = require("fs");
+// const fs = require("fs");
+require('dotenv').config()
 const cloudinary = require("cloudinary").v2;
 
-cloudinary.config({
-  cloud_name: "jonathancloudinary",
-  api_key: "333687443411313",
-  api_secret: "P2sTmibIMeN-9N4YEoJL3uByrQQ",
-});
+// const images = [];
 
-const images = [];
+// ! Commented out as there's no need to run the upload more than once
+// fs.readdir("./assets/private/", (err, files) => {
+//   images.push(...files);
+//   console.log(images);
 
-fs.readdir("./assets/private/", (err, files) => {
-  images.push(...files);
-  console.log(images);
+//   images.forEach((index) => {
+//     cloudinary.uploader.upload(
+//       `./assets/private/${index}`,
+//       {
+//         public_id: `privateImageGallery/${index}`,
+//         unique_filename: false,
+//         use_filename: true,
+//         invalidate: true,
+//         resource_type: "image",
+//         overwrite: true,
+//         type: 'private'
+//       },
+//       function (error, success) {
+//         console.log(error, success);
+//       }
+//     );
+//   });
+// });
 
-  images.forEach((index) => {
-    cloudinary.uploader.upload(
-      `./assets/private/${index}`,
-      {
-        public_id: `privateImageGallery/${index}`,
-        unique_filename: false,
-        use_filename: true,
-        invalidate: true,
-        resource_type: "image",
-        overwrite: true,
-        type: 'private'
-      },
-      function (error, success) {
-        console.log(error, success);
-      }
-    );
-  });
-});
+const modal = document.getElementById('img_modal')
