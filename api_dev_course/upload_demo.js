201442975a26874239eb40a94f5d77ad0f78706c
Jonathan Sexton <joncsexton@gmail.com>
2023-12-26 09:20:21 -0600

    updates to async

--- api_dev_course/upload_demo.js
+++ api_dev_course/upload_demo.js
@@ -2,28 +2,43 @@ require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
 const images = [
-  "assets/images/baseball_hat_transparent.png",
-  "assets/images/black_hole.webp",
-  "assets/images/space_rocks.webp",
+  'assets/images/baseball_hat_transparent.png',
+  'assets/images/black_hole.webp',
+  'assets/images/space_rocks.webp',
 ];
+// Original working code
+// images.forEach((image) => {
+//   cloudinary.uploader.upload(
+//     image,
+//     {
+//       invalidate: true,
+//       resource_type: "image",
+//       overwrite: true,
+//       use_filename: true
+//     },
+//     function (error, success) {
+//       if (error) {
+//         console.error("There was an error on upload: ", error);
+//       } else {
+//         console.log("Success: ", success);
+//       }
+//     }
+//   );
 
 images.forEach((image) => {
-  cloudinary.uploader.upload(
-    image,
-    {
+  cloudinary.uploader
+    .upload(image, {
       invalidate: true,
-      resource_type: "image",
+      resource_type: 'image',
       overwrite: true,
-      use_filename: true
-    },
-    function (error, success) {
-      if (error) {
-        console.error("There was an error on upload: ", error);
-      } else {
-        console.log("Success: ", success);
-      }
-    }
-  );
+      use_filename: true,
+    })
+    .then((result) => {
+      console.log('Success:', result);
+    })
+    .catch((error) => {
+      console.log('There was an error:', error);
+    });
 });
 
 // cloudinary.uploader.upload('assets/images/black_hole.webp',
